f66d34e7a3a71379a014dbc14093223f0abe567b
mariaziui <azigamarita@gmail.com>
2026-03-30 14:18:55 -0400

    implemented functionality for all the buttons

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+const clearEntry = document.querySelector('.clear-entry');
+const clear = document.querySelectorAll('.clear');
+
+const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
+
+const equals = document.querySelector('.equals');
+
+const numScreen = document.querySelector('input');
+
+const list = document.querySelector('ul');
+
+let entries = [];
+
+let num1 = '';
+let num2 = '';
+let currentOperator = '';
+
 function add(x,y) {
     return x + y;
 }
 
 function subtract(x,y) {
-    return x + y;
+    return x - y;
 }
 
 function multiply(x,y) {
@@ -14,15 +32,59 @@ function divide(x,y) {
     return x / y;
 }
 
+
 function operate(num1, num2, operator) {
     switch (operator) {
         case "+":
-            add(num1,num2);
+            return add(num1,num2);
         case "-":
-            subtract(num1,num2);
+            return subtract(num1,num2);
         case "*":
-            multiply(num1,num2);
+            return multiply(num1,num2);
         case "/":
-            divide(num1,num2);
+            return divide(num1,num2);
     }
 }
+
+function entryList(entries) {
+    
+}
+
+numScreen.value = '';
+
+digits.forEach((digit) => {
+    digit.addEventListener('click', () => {
+        if (currentOperator === '') {
+            numScreen.value += digit.innerHTML;
+            num1 = parseFloat(numScreen.value);
+        } else {
+            numScreen.value += digit.innerHTML;
+            num2 = parseFloat(numScreen.value);
+        }
+    });
+});
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if (num1 !== '') {
+            currentOperator = operator.innerHTML;
+            numScreen.value = '';
+        }
+    })
+});
+
+equals.addEventListener("click", () => {
+    let operation = `${num1} ${currentOperator} ${num2}`;
+    entries.push(operation);
+    numScreen.value = operate(num1, num2, currentOperator);
+});
+
+clearEntry.addEventListener("click", () => {
+    numScreen.value = '';
+});
+
+clear.addEventListener("click", () => {
+    numScreen.value = '';
+
+    entries.forEach((entry) => entries.pop(entry));
+})
