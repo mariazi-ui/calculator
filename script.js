3f11d83e93d8b80de1c31341e6d1f3453ade6d62
mariaziui <azigamarita@gmail.com>
2026-03-31 11:15:16 -0400

    finalized javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const clearEntry = document.querySelector('.clear-entry');
-const clear = document.querySelectorAll('.clear');
+const clear = document.querySelector('.clear');
 
 const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
@@ -16,75 +16,79 @@ let num1 = '';
 let num2 = '';
 let currentOperator = '';
 
-function add(x,y) {
-    return x + y;
-}
-
-function subtract(x,y) {
-    return x - y;
-}
-
-function multiply(x,y) {
-    return x * y;
-}
-
-function divide(x,y) {
-    return x / y;
-}
-
-
 function operate(num1, num2, operator) {
     switch (operator) {
         case "+":
-            return add(num1,num2);
+            return num1 + num2;
         case "-":
-            return subtract(num1,num2);
+            return num1 - num2;
         case "*":
-            return multiply(num1,num2);
+            return num1 * num2;
         case "/":
-            return divide(num1,num2);
+            return num1 / num2;
     }
 }
 
 function entryList(entries) {
-    
+    list.innerHTML = '';
+    for (let i = 0; i < entries.length; i++) {
+        let li = document.createElement('li');
+        li.innerHTML = entries[i];
+        list.appendChild(li);        
+    }    
 }
 
-numScreen.value = '';
+window.addEventListener("load", () => {
+    numScreen.value = '';
 
-digits.forEach((digit) => {
-    digit.addEventListener('click', () => {
-        if (currentOperator === '') {
-            numScreen.value += digit.innerHTML;
-            num1 = parseFloat(numScreen.value);
-        } else {
-            numScreen.value += digit.innerHTML;
-            num2 = parseFloat(numScreen.value);
-        }
+    digits.forEach((digit) => {
+        digit.addEventListener('click', () => {
+            if (currentOperator === '') {
+                numScreen.value += digit.innerHTML;
+                num1 = parseFloat(numScreen.value);
+            } else {
+                numScreen.value += digit.innerHTML;
+                num2 = parseFloat(numScreen.value);
+            }
+        });
+    });
+
+    operators.forEach((operator) => {
+        operator.addEventListener('click', () => {
+            if (num1 !== '') {
+                currentOperator = operator.innerHTML;
+                numScreen.value = '';
+            }
+        });
+    });
+
+    equals.addEventListener("click", () => {
+        let result = operate(num1, num2, currentOperator);
+        let operation = `${num1} ${currentOperator} ${num2} = ${result}`;
+        entries.push(operation);
+
+        list.innerHTML = '';
+        entryList(entries);
+
+        numScreen.value = result;
+
+        num1 = result;
+        num2 = '';
+        currentOperator = '';
+    });
+
+    clearEntry.addEventListener("click", () => {
+        numScreen.value = '';
+    });
+
+    clear.addEventListener("click", () => {
+        numScreen.value = '';
+
+        entries = [];
+        list.innerHTML = '';
+
+        num1 = '';
+        num2 = '';
+        currentOperator = '';
     });
 });
-
-operators.forEach((operator) => {
-    operator.addEventListener('click', () => {
-        if (num1 !== '') {
-            currentOperator = operator.innerHTML;
-            numScreen.value = '';
-        }
-    })
-});
-
-equals.addEventListener("click", () => {
-    let operation = `${num1} ${currentOperator} ${num2}`;
-    entries.push(operation);
-    numScreen.value = operate(num1, num2, currentOperator);
-});
-
-clearEntry.addEventListener("click", () => {
-    numScreen.value = '';
-});
-
-clear.addEventListener("click", () => {
-    numScreen.value = '';
-
-    entries.forEach((entry) => entries.pop(entry));
-})
